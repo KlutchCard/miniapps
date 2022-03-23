5eebe361c8c7ea1bc538dfdafe1c21bd98bd18b6
VictorBarros <victorbarros1130@gmail.com>
2022-03-23 20:08:27 -0300

    add create recipe install test

--- categorization/backend/src/test/test-webhook.ts
+++ categorization/backend/src/test/test-webhook.ts
@@ -1,12 +1,25 @@
 import {expect} from "chai";
 import {handleWebhook} from "../app.js"
 
-const recipeInstall = process.env.testRecipeInstallId
+const recipeInstallId = process.env.testRecipeInstallId
+
+const exampleRecipeInstallCreatedEvent = {
+  "principal": {
+    "_alloyCardType": "com.alloycard.core.entities.recipe.RecipeInstall",
+    "entityID": recipeInstallId
+  },
+  "event": {
+    "_alloyCardType": "com.alloycard.core.entities.recipe.RecipeInstallCreatedEvent",
+    "createdAt": 1629129978,
+    "eventId": "xpto"
+  },
+  "webhookUrl": "https://dn0xoemayc.execute-api.us-west-2.amazonaws.com/klutch-webhook"
+}
 
 const exampleTransactionEvent = {
     "principal": {
       "_alloyCardType": "com.alloycard.core.entities.recipe.RecipeInstall",
-      "entityID": recipeInstall
+      "entityID": recipeInstallId
     },
     "event": {
       "_alloyCardType": "com.alloycard.core.entities.transaction.TransactionCreatedEvent",
@@ -21,6 +34,14 @@ const exampleTransactionEvent = {
   }
 
 describe('Tests SubscriptionManager', function () {
+  it.only("RecipeInstallCreated event received on Webhook", async () => {
+    const transaction = JSON.stringify(exampleRecipeInstallCreatedEvent)
+    var resp = await handleWebhook({
+      body: transaction
+    } as any)
+    expect(resp.statusCode).to.be.equal(200)
+  })
+
 
     it("Transaction received on Webhook", async () => {        
         const transaction = JSON.stringify(exampleTransactionEvent)

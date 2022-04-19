9a6cbfae1a219bf0328f67a683bad04ca38d0fe4
VictorBarros <victorbarros1130@gmail.com>
2022-04-18 21:00:28 -0300

    check klutch server health

--- if-then/backend/src/routes.js
+++ if-then/backend/src/routes.js
@@ -21,30 +21,29 @@ router.get("/health", async (req, resp) => {
 
   let responseStatus = httpStatus.OK
   let services = {
-    // klutchServer: {
-    //   success: true,
-    //   errorMessage: null,
-    // },
+    klutchServer: {
+      success: true,
+      errorMessage: null,
+    },
     database: {
       success: true,
       errorMessage: null,
     },
   }
 
-  // TODO fix klutchServerUrl
-  // await axios({ method: 'get', url: `${klutchServerUrl}/healthcheck`, })
-  //   .catch(function (error) {
-  //     services.klutchServer.success = false
-  //     services.klutchServer.errorMessage = "klutch server comunication fail"
-  //     responseStatus = httpStatus.SERVICE_UNAVAILABLE
-  //     console.log(services.klutchServer.errorMessage, error)
-  //   })
+  await axios({ method: 'get', url: `${klutchServerUrl}/healthcheck`, })
+    .catch(function (error) {
+      services.klutchServer.success = false
+      services.klutchServer.errorMessage = "klutch server comunication fail"
+      responseStatus = httpStatus.SERVICE_UNAVAILABLE
+      console.log(services.klutchServer.errorMessage, error)
+    })
 
   if (Automation.collection.conn._readyState !== 1) {
     services.database.success = false
     services.database.errorMessage = "database connection fail"
     responseStatus = httpStatus.SERVICE_UNAVAILABLE
-    // console.log(services.klutchServer.errorMessage)
+    console.log(services.klutchServer.errorMessage)
   }
 
   return resp.status(responseStatus).json({ services, version })

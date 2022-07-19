2a5de9975a28b3e964b750d45ee533a4036fc0b7
VictorBarros <victorbarros1130@gmail.com>
2022-07-19 16:53:12 -0300

    init webhook

--- MyMiniappTutorial/backend/index.ts
+++ MyMiniappTutorial/backend/index.ts
@@ -1,8 +1,11 @@
+import { GraphQLService, RecipesService, KlutchJS, RecipePanelSize, } from "@klutch-card/klutch-js"
 import express, { NextFunction, Request, Response } from "express"
 import axios from "axios"
+import { BuildJWTToken } from "./helper"
+import { PORT, RECIPEINSTALL_CREATED_EVENT, SERVER_URL } from "./config"
 
-const PORT = 3004
-const SERVER_URL = "https://sandbox.klutchcard.com"
+
+KlutchJS.configure({ serverUrl: `${SERVER_URL}/graphql` })
 
 const middleware = (req: Request, res: Response, next: NextFunction) => {
     console.log(`${new Date().toISOString()} ${req.method} "${req.url}" started`)
@@ -26,8 +29,33 @@ const healthCheckController = async (req: Request, res: Response) => {
     res.status(responseStatus).json({ services })
 }
 
+const webhookController = async (req: Request, res: Response) => {
+    const { event, principal } = req.body
+
+    if (!event || !principal) return res.status(400).json()
+
+    const recipeInstallId = principal.entityID
+    const eventType = event._alloyCardType
+
+    try {
+        GraphQLService.setAuthToken(BuildJWTToken())
+        GraphQLService.setAuthToken(await RecipesService.getRecipeInstallToken(recipeInstallId))
+
+        if (eventType === RECIPEINSTALL_CREATED_EVENT) {
+            console.log("adding home panel", { recipeInstallId })
+            await RecipesService.addPanel(recipeInstallId, "/templates/Home.template", {}, undefined, undefined, RecipePanelSize.LARGE)
+        }
+    } catch (err) {
+        console.log({ err, recipeInstallId })
+        return res.status(503).json({ error: "Fail to request to graphql api" })
+    }
+
+    return res.status(200).json()
+}
+
 express()
     .use(middleware)
     .use(express.json())
     .get('/healthcheck', healthCheckController)
+    .post('/webhook', webhookController)
     .listen(PORT, () => console.log(`runnnig on port ${PORT}`))
